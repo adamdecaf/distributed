b8929fb288b5d47dd6f1732b262b4a3c97ba6ac6
Adam Shannon <adam@ashannon.us>
2015-11-14 11:28:22 -0600

    server: Add health route to boot routes

--- server/src/main/scala/Boot.scala
+++ server/src/main/scala/Boot.scala
@@ -1,14 +1,18 @@
 package org.decaf.distributed.server
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
 import org.decaf.distributed.server.aggregation.{AggregationRoutes, PingRoute}
+import org.decaf.distributed.server.health.HealthRoute
 
 object Boot extends App
     with HttpServer
     with AggregationRoutes
+    with HealthRoute
     with PingRoute {
 
   def routes: Route =
-    pingRoute()
+    pingRoute() ~
+    healthRoute()
     // aggregationRoutes()
 
   println("Starting 'server'")
